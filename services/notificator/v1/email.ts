acf626340c0e6618c5cfa5440e91627ba2addfdd
Fabio FENOGLIO <fabio@numeral.io>
2024-04-17 19:11:34 +0200

    added managed errors

--- services/notificator/v1/email.ts
+++ services/notificator/v1/email.ts
@@ -1,14 +1,18 @@
+export type EmailChannel = 'sendgrid/default' | 'postmark/default';
 
 export type SendEmailV1Input = {
   from: string;
   fromName?: string;
   to: string;
+  toName?: string;
   subject: string;
   text?: string;
   html?: string;
   inReplyTo?: string;
   replyTo?: string;
   cc?: string[];
+  bcc?: string[];
+  channel?: EmailChannel;
 }
 
 export type SendEmailV1Output = {
@@ -19,12 +23,15 @@ export type SendSystemNotificationEmailV1Input = {
   from?: string;
   fromName?: string;
   to?: string;
+  toName?: string;
   subject?: string;
   text?: string;
   html?: string;
   inReplyTo?: string;
   replyTo?: string;
   cc?: string[];
+  bcc?: string[];
+  channel?: EmailChannel;
 }
 
 export type SendSystemNotificationEmailV1Output = {
